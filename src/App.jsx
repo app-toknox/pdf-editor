3b9042a819aaf2d51531d7380500ce7d8a846414
BorsL <126157610+BorsL@users.noreply.github.com>
2025-05-13 13:03:16 +0200

    removing code dnd

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import { Layout } from "./components/layout";
 import { Home } from "./pages/Home";
-import { DragAndDropProvider } from "./providers/drag-and-drop-provider";
 
 const App = () => {
   return (
-    <DragAndDropProvider>
-      <Layout>
-        <Home />
-      </Layout>
-    </DragAndDropProvider>
+    <Layout>
+      <Home />
+    </Layout>
   );
 };
 
--- src/config/constant.js
+++
@@ -1,3 +0,0 @@
-export const BASE_POSITION_X = 20;
-export const BASE_POSITION_Y = 80;
-export const VERTICAL_DISTANCE = 60;
